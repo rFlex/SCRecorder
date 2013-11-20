607734dd8593cc2678c22ebbc64644d9232c4088
xhzengAIB <xhzengAIB@gmail.com>
2013-11-20 15:12:13 +0000

    Add SCBlurOverlayView show blurred images, UIPinchGestureRecognizer and UIPanGestureRecognizer control SCBlurOverlayView radius size and the location of the blur blur·
    Question
    1、I don't know how to according to SCBlurOverlayView blur the radius of
    the size and the location of the blur blur picture
    2、How to add the image filter?

---
+++ SCAudioVideoRecorderExampleIOS/SCBlurOverlayView.h
@@ -0,0 +1,14 @@
+//
+//  XHBlurOverlayView.h
+//  iyilunba
+//
+//  Created by 曾 宪华 on 13-11-19.
+//  Copyright (c) 2013年 曾 宪华 开发团队(http://iyilunba.com ). All rights reserved. 本人QQ 543413507
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCBlurOverlayView : UIView
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) CGPoint circleCenter;
+@end
--- SCAudioVideoRecorderExampleIOS/SCImageViewDisPlayViewController.m
+++ SCAudioVideoRecorderExampleIOS/SCImageViewDisPlayViewController.m
@@ -7,21 +7,28 @@
 //
 
 #import "SCImageViewDisPlayViewController.h"
+#import "SCBlurOverlayView.h"
 
 @interface SCImageViewDisPlayViewController ()
-
+@property (strong, nonatomic) SCBlurOverlayView *blurOverlayView;
+@property (assign, nonatomic) BOOL hasBlur;
+@property (assign, nonatomic) CGFloat radius;
 @end
 
 @implementation SCImageViewDisPlayViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+#pragma mark - Propertys
+
+- (SCBlurOverlayView *)blurOverlayView {
+    if (!_blurOverlayView) {
+        _blurOverlayView = [[SCBlurOverlayView alloc] initWithFrame:self.disPlayImageView.bounds];
+        _blurOverlayView.alpha = 0;
+        [self.disPlayImageView addSubview:self.blurOverlayView];
     }
-    return self;
+    return _blurOverlayView;
 }
+
+#pragma mark - Left cycle init
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
@@ -33,10 +40,26 @@
     self.navigationController.navigationBarHidden = YES;
 }
 
+- (void)_setupGesture {
+    self.disPlayImageView.userInteractionEnabled = YES;
+    
+    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchGestureRecognizerHandle:)];
+    [self.disPlayImageView addGestureRecognizer:pinchGestureRecognizer];
+    
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureRecognizerHandle:)];
+    panGestureRecognizer.minimumNumberOfTouches = 1;
+    [self.disPlayImageView addGestureRecognizer:panGestureRecognizer];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self _setupGesture];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Blur", @"") style:UIBarButtonItemStyleBordered target:self action:@selector(toggleBlur)];
+    
     if (self.photo)
         self.disPlayImageView.image = self.photo;
 }
@@ -45,6 +68,87 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIGestureRecognizer handle
+
+-(void)panGestureRecognizerHandle:(UIGestureRecognizer *) sender {
+    if (self.hasBlur) {
+        CGPoint tapPoint = [sender locationInView:sender.view];
+        if ([sender state] == UIGestureRecognizerStateBegan) {
+            [self showBlurOverlay:YES];
+        }
+        
+        if ([sender state] == UIGestureRecognizerStateBegan || [sender state] == UIGestureRecognizerStateChanged) {
+            [self.blurOverlayView setCircleCenter:tapPoint];
+        }
+        
+        if([sender state] == UIGestureRecognizerStateEnded){
+            [self showBlurOverlay:NO];
+        }
+    }
+}
+
+-(void)pinchGestureRecognizerHandle:(UIPinchGestureRecognizer *) sender {
+    if (self.hasBlur) {
+        CGPoint midpoint = [sender locationInView:sender.view];
+        if ([sender state] == UIGestureRecognizerStateBegan) {
+            [self showBlurOverlay:YES];
+        }
+        
+        if ([sender state] == UIGestureRecognizerStateBegan || [sender state] == UIGestureRecognizerStateChanged) {
+            self.blurOverlayView.circleCenter = CGPointMake(midpoint.x, midpoint.y);
+            CGFloat radius = MAX(MIN(sender.scale*self.radius, 0.6f), 0.15f);
+            self.blurOverlayView.radius = radius*CGRectGetWidth(self.view.bounds);
+            self.radius = radius;
+            sender.scale = 1.0f;
+        }
+        
+        if ([sender state] == UIGestureRecognizerStateEnded) {
+            [self showBlurOverlay:NO];
+        }
+    }
+}
+
+#pragma mark - Blur
+
+- (void)toggleBlur {
+    
+    if (self.hasBlur) {
+        self.hasBlur = NO;
+        [self showBlurOverlay:NO];
+    } else {
+        self.hasBlur = YES;
+        [self flashBlurOverlay];
+    }
+}
+
+-(void) showBlurOverlay:(BOOL)show{
+    if(show){
+        [UIView animateWithDuration:0.2 delay:0 options:0 animations:^{
+            self.blurOverlayView.alpha = 0.6;
+        } completion:^(BOOL finished) {
+            
+        }];
+    }else{
+        [UIView animateWithDuration:0.35 delay:0.2 options:0 animations:^{
+            self.blurOverlayView.alpha = 0;
+        } completion:^(BOOL finished) {
+            
+        }];
+    }
+}
+
+-(void) flashBlurOverlay {
+    [UIView animateWithDuration:0.2 delay:0 options:0 animations:^{
+        self.blurOverlayView.alpha = 0.6;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.35 delay:0.2 options:0 animations:^{
+            self.blurOverlayView.alpha = 0;
+        } completion:^(BOOL finished) {
+            
+        }];
+    }];
 }
 
 @end
