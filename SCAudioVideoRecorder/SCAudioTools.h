e41148c4dedba7f7ff67430480931c830d194368
xhzengAIB <xhzengAIB@gmail.com>
2013-12-17 10:08:01 +0000

    Merge branch 'master' of https://github.com/rFlex/SCAudioVideoRecorder

--- SCAudioVideoRecorder/SCAudioTools.h
+++ SCAudioVideoRecorder/SCAudioTools.h
@@ -19,5 +19,6 @@
 #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
 + (void) overrideCategoryMixWithOthers;
 #endif
++ (void)mixAudio:(AVAsset*)audioAsset startTime:(CMTime)startTime withVideo:(NSURL*)inputUrl affineTransform:(CGAffineTransform)affineTransform toUrl:(NSURL*)outputUrl outputFileType:(NSString*)outputFileType withMaxDuration:(CMTime)maxDuration withCompletionBlock:(void(^)(NSError *))completionBlock;
 
 @end
