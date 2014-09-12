f0be35cef6d487700e08c2949fb38c7ff671646f
Isaiah J Turner <github.com@isaiahjturner.com>
2014-09-12 11:32:32 -0400

    fixes issue with interface orientation
    Device orientation was used even when the interface displayed the
    recording in landscape mode for landscape only applications.

--- Library/Sources/SCRecorder.m
+++ Library/Sources/SCRecorder.m
@@ -794,7 +794,7 @@
     AVCaptureVideoOrientation videoOrientation = _videoOrientation;
     
     if (_autoSetVideoOrientation) {
-        UIDeviceOrientation deviceOrientation = [[UIDevice currentDevice] orientation];
+        UIDeviceOrientation deviceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
         
         switch (deviceOrientation) {
             case UIDeviceOrientationLandscapeLeft:
