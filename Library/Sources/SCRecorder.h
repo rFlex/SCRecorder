fcad736f2abf3876eadfe8e026aa663670a5df35
Max Mashkov <mashkovmax@gmail.com>
2014-12-25 13:43:40 +0200

    fixed 'recordedRatio' method missing in interface

--- Library/Sources/SCRecorder.h
+++ Library/Sources/SCRecorder.h
@@ -183,7 +183,7 @@ typedef NS_ENUM(NSInteger, SCFlashMode) {
  this will contains a float between 0 and 1 representing the
  recorded ratio on the current record session, 1 being fully recorded.
  */
-@property (readonly, nonatomic) CGFloat ratioRecorded;
+@property (readonly, nonatomic) CGFloat recordedRatio;
 
 /**
  If enabled, the recorder will initialize the recordSession and create the record segments
