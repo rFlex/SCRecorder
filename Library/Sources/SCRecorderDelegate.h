3a191eab8191ba324eaca431c9cd47dfad8d9fec
Simon CORSIN <simon@corsin.me>
2015-03-25 14:48:39 -0700

    Fixed missing imports.

--- Library/Sources/SCRecorderDelegate.h
+++ Library/Sources/SCRecorderDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 #import "SCRecorder.h"
 
 typedef NS_ENUM(NSInteger, SCFlashMode) {
--- Library/Sources/SCRecorderTools.h
+++ Library/Sources/SCRecorderTools.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface SCRecorderTools : NSObject
 
