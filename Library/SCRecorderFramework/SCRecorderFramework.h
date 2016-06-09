741e2d1f3ce83dcc0be2df638904a5ffbdf2edc3
Evan Kimia <evan.kimia@icloud.com>
2016-06-09 01:05:55 -0700

    expose all needed public headers

--- Library/SCRecorderFramework/SCRecorderFramework.h
+++ Library/SCRecorderFramework/SCRecorderFramework.h
@@ -14,7 +14,42 @@ FOUNDATION_EXPORT double SCRecorderFrameworkVersionNumber;
 //! Project version string for SCRecorderFramework.
 FOUNDATION_EXPORT const unsigned char SCRecorderFrameworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SCRecorderFramework/PublicHeader.h>
+// Recorder.
 
 #import <SCRecorder/SCRecorder.h>
 #import <SCRecorder/SCRecordSession.h>
+#import <SCRecorder/SCVideoConfiguration.h>
+#import <SCRecorder/SCAudioConfiguration.h>
+#import <SCRecorder/SCMediaTypeConfiguration.h>
+#import <SCRecorder/SCPhotoConfiguration.h>
+#import <SCRecorder/SCRecordSession.h>
+#import <SCRecorder/SCRecordSessionSegment.h>
+#import <SCRecorder/SCRecordDelegate.h>
+
+// ToolsView.
+
+#import <SCRecorder/SCRecorderFocusTargetView.h>
+#import <SCRecorder/SCRecorderToolsView.h>
+
+
+// FilterDisplayers.
+
+#import <SCRecorder/SCSwipeableFilterView.h>
+#import <SCRecorder/SCImageView.h>
+
+// Players.
+
+#import <SCRecorder/SCPlayer.h>
+#import <SCRecorder/SCVideoPlayerView.h>
+
+// Tools.
+
+#import <SCRecorder/SCSampleBufferHolder.h>
+#import <SCRecorder/SCAssetExportSession.h>
+#import <SCRecorder/SCAudioTools.h>
+#import <SCRecorder/SCRecorderTools.h>
+#import <SCRecorder/SCProcessingQueue.h>
+#import <SCRecorder/SCIOPixelBuffers.h>
+
+// Filters.
+#import <SCRecorder/SCFilter.h>
