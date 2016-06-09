4ebaa65c516542eac16a6c951588187bb492adc1
Evan Kimia <evan.kimia@icloud.com>
2016-06-09 00:44:11 -0700

    SCRecordSession should be public

--- Library/SCRecorderFramework/SCRecorderFramework.h
+++ Library/SCRecorderFramework/SCRecorderFramework.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char SCRecorderFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SCRecorderFramework/PublicHeader.h>
 
 #import <SCRecorder/SCRecorder.h>
+#import <SCRecorder/SCRecordSession.h>
